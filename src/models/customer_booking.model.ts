0647bce5b73ce0bd718feaae780b4d1f66a4671b
ishgervais <ishimwegervais11@gmail.com>
2021-10-18 00:32:29 +0200

    db connected successfully

---
+++ src/models/customer_booking.model.ts
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+import { ENotification } from '../types/enums';
+const CustomerBookingSchema = new mongoose.Schema({
+    names:{
+        type:String,
+        minLength:[3,'The names should be atleast 3 characters'],
+        required:true
+    },
+    email:{
+        type:String,
+        required:true    
+    },
+    phone_number:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    
+    date_id:{
+        type:mongoose.Types.ObjectId,
+        required:true
+    },
+    status:{
+        type:String,
+        default:ENotification.UNREAD,
+        enum:[ENotification.UNREAD,ENotification.READ]
+
+    }
+},{
+    timestamps:true,
+    toJSON:{
+        virtuals:true
+    },
+    toObject:{
+        virtuals:true
+    }
+})
+
+CustomerBookingSchema.virtual('c_booking',{
+    ref:"Visit",
+    localField:"date_id",
+    foreignField:'_id',
+    justOne:true
+})
+
+
+const CustomerBooking = mongoose.model('CustomerBooking', CustomerBookingSchema);
+export {CustomerBooking}
